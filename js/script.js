6f4bf76f79d5e218e14fee7534ad15760af1370a
Zach Boyce <boyce.141@osu.edu>
2020-11-13 21:04:44 -0500

    ohio2electricboogaloo

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function script(){
     
-    var mymap = L.map('mapid').setView([40.4173, -82.9071], 8);
+    var mymap = L.map('mapid').setView([39.9612, -82.9988], 8);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
@@ -10,7 +10,7 @@ function script(){
         accessToken: 'pk.eyJ1IjoiemFjaGZiIiwiYSI6ImNraGd6bDAwaTA1Mm8ydXBpOHc2YXFpa2oifQ.ApIhHDnfpfXaG5uE9A3SSg'
     }).addTo(mymap);
 
-    var circle = L.circle([40.4173, -82.9071], {
+    var circle = L.circle([39.9612, -82.9988], {
         color: 'red',
         fillColor: '#f03',
         fillOpacity: 0.5,
