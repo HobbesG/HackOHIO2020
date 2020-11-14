9a954cee5478e21a578f8f50679888f8cd37bcec
Zach Boyce <boyce.141@osu.edu>
2020-11-13 21:46:32 -0500

    line

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ function script(){
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
+        minZoom: 2,
         id: 'mapbox/streets-v11',
         tileSize: 512,
         zoomOffset: -1,
@@ -16,5 +17,14 @@ function script(){
         fillOpacity: 0.5,
         radius: 50000
     }).addTo(mymap);
+
+    var latlngs = [
+        [45.51, -122.68],
+        [37.77, -122.43],
+        [34.04, -118.2]
+    ];
+
+    var polygon = L.polyline(latlngs, {color: 'red'}).addTo(mymap);
+    
   
 }
